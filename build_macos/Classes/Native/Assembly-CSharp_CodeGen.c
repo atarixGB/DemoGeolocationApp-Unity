70283208f3cfe508a4f7824793eaf116fa9a4497
Mikael Gilbert <leakim_gilbert9@hotmail.com>
2023-04-26 19:03:08 -0400

    Add xcodeproject

---
+++ build_macos/Classes/Native/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,153 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 GPS GPS::get_Instance()
+extern void GPS_get_Instance_m1D5707610052E85B0B999E9BC31376A0250D72AC (void);
+// 0x00000002 System.Void GPS::set_Instance(GPS)
+extern void GPS_set_Instance_m9EFFCCBB4E801683289241F15D9B75C4D264EC0A (void);
+// 0x00000003 System.Void GPS::Start()
+extern void GPS_Start_m1A862A601DE8567A4784509DA15584ED5A0101F9 (void);
+// 0x00000004 System.Void GPS::Update()
+extern void GPS_Update_mBE59FCFF4CC1929C672A8A56D5D78890FC268917 (void);
+// 0x00000005 System.Void GPS::OnDestroy()
+extern void GPS_OnDestroy_mD6CA27972773AADDB4DFEC52E1FEFDB46E0BCCD9 (void);
+// 0x00000006 System.Collections.IEnumerator GPS::getGPSCoordinates()
+extern void GPS_getGPSCoordinates_mA7AF050503FE06586138243C6BD8353760E07E11 (void);
+// 0x00000007 System.Void GPS::.ctor()
+extern void GPS__ctor_mD1DE1233CA5BBE1CB8BACA287B8594BF7B010F59 (void);
+// 0x00000008 System.Void GPS/<getGPSCoordinates>d__17::.ctor(System.Int32)
+extern void U3CgetGPSCoordinatesU3Ed__17__ctor_m17B61356F2C09872A8F22747A36E0F3416F74905 (void);
+// 0x00000009 System.Void GPS/<getGPSCoordinates>d__17::System.IDisposable.Dispose()
+extern void U3CgetGPSCoordinatesU3Ed__17_System_IDisposable_Dispose_m187F201E9DEABFC677345FAE91775D333EB69991 (void);
+// 0x0000000A System.Boolean GPS/<getGPSCoordinates>d__17::MoveNext()
+extern void U3CgetGPSCoordinatesU3Ed__17_MoveNext_mAAA4B8FB31974AE317FBC773922FCCD47259659C (void);
+// 0x0000000B System.Object GPS/<getGPSCoordinates>d__17::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CgetGPSCoordinatesU3Ed__17_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m8CD621290856B081B9004528C3D8FC165DB203C6 (void);
+// 0x0000000C System.Void GPS/<getGPSCoordinates>d__17::System.Collections.IEnumerator.Reset()
+extern void U3CgetGPSCoordinatesU3Ed__17_System_Collections_IEnumerator_Reset_mE7A7EBC2CEF2028A2E3D77912622D5B96745D4BE (void);
+// 0x0000000D System.Object GPS/<getGPSCoordinates>d__17::System.Collections.IEnumerator.get_Current()
+extern void U3CgetGPSCoordinatesU3Ed__17_System_Collections_IEnumerator_get_Current_mB3A734F19EDB4ECA4D4943B08EA3A2A4E6820030 (void);
+// 0x0000000E System.Double Helper::degreesToRadians(System.Double)
+extern void Helper_degreesToRadians_m90682E512BBF866B7CF7AC81B5AD3BD6C28F3B6B (void);
+// 0x0000000F System.Double Helper::radiansToDegrees(System.Double)
+extern void Helper_radiansToDegrees_m0B1D570588853C6720035C4D75D90EC19CE1DB0C (void);
+// 0x00000010 System.Tuple`2<System.Double,System.Double> Helper::getNewGPSCoordinate(System.Double,System.Double,System.Double,System.Double)
+extern void Helper_getNewGPSCoordinate_m77327B95A023F034A47BFBC654D96ABC6308F85C (void);
+// 0x00000011 System.Double Helper::distanceBetweenTwoGPSCoordinates(System.Double,System.Double,System.Double,System.Double)
+extern void Helper_distanceBetweenTwoGPSCoordinates_m20E9BBD75BC72FEFFF344375A747F061516DD8B7 (void);
+// 0x00000012 System.Double Helper::getBearing(System.Double,System.Double,System.Double,System.Double)
+extern void Helper_getBearing_m6283597EDDF421069571C6DE0398A70FFB4C520D (void);
+// 0x00000013 System.Int32 Helper::getElapsedSecondsFromUnixEpoch()
+extern void Helper_getElapsedSecondsFromUnixEpoch_m0EBAD1A27A9D87F6AE1C68D9397ABFE7FE834D8C (void);
+// 0x00000014 System.Void UI::Start()
+extern void UI_Start_m36F3E11677AB5677BD3A76B5865E90CD3609183A (void);
+// 0x00000015 System.Void UI::Update()
+extern void UI_Update_m7F9212980C571870E0700BAB400E09111370F6ED (void);
+// 0x00000016 System.Void UI::displayTargetLocation()
+extern void UI_displayTargetLocation_m3CD585034947786F9B68DE5B3232B9E3CB7EE297 (void);
+// 0x00000017 System.Void UI::displayDistanceFromTarget()
+extern void UI_displayDistanceFromTarget_m55E8E4562910E3C1FA5E6BF2E405AC008D92CA37 (void);
+// 0x00000018 System.Void UI::displayCurrentLocation()
+extern void UI_displayCurrentLocation_m4590C00079FBAABFC05667697A1912DA810C1B3F (void);
+// 0x00000019 System.Void UI::rotateArrow()
+extern void UI_rotateArrow_m2E42F991980B65EA55CDDFED91C6F12FA1B3DE94 (void);
+// 0x0000001A System.Void UI::displayCurrentDeviceOrientation()
+extern void UI_displayCurrentDeviceOrientation_mEC974F1E8CCD61C31249E3BDE96BBACFDD82738D (void);
+// 0x0000001B System.Void UI::toggleGreenSquare(System.Double)
+extern void UI_toggleGreenSquare_mA2F16CF3402F9E407E95D0AE65214D7B748B66D6 (void);
+// 0x0000001C System.Void UI::.ctor()
+extern void UI__ctor_m177FCA8E7C6A148BFF6FED9F758B3396F25FDFBB (void);
+static Il2CppMethodPointer s_methodPointers[28] = 
+{
+	GPS_get_Instance_m1D5707610052E85B0B999E9BC31376A0250D72AC,
+	GPS_set_Instance_m9EFFCCBB4E801683289241F15D9B75C4D264EC0A,
+	GPS_Start_m1A862A601DE8567A4784509DA15584ED5A0101F9,
+	GPS_Update_mBE59FCFF4CC1929C672A8A56D5D78890FC268917,
+	GPS_OnDestroy_mD6CA27972773AADDB4DFEC52E1FEFDB46E0BCCD9,
+	GPS_getGPSCoordinates_mA7AF050503FE06586138243C6BD8353760E07E11,
+	GPS__ctor_mD1DE1233CA5BBE1CB8BACA287B8594BF7B010F59,
+	U3CgetGPSCoordinatesU3Ed__17__ctor_m17B61356F2C09872A8F22747A36E0F3416F74905,
+	U3CgetGPSCoordinatesU3Ed__17_System_IDisposable_Dispose_m187F201E9DEABFC677345FAE91775D333EB69991,
+	U3CgetGPSCoordinatesU3Ed__17_MoveNext_mAAA4B8FB31974AE317FBC773922FCCD47259659C,
+	U3CgetGPSCoordinatesU3Ed__17_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m8CD621290856B081B9004528C3D8FC165DB203C6,
+	U3CgetGPSCoordinatesU3Ed__17_System_Collections_IEnumerator_Reset_mE7A7EBC2CEF2028A2E3D77912622D5B96745D4BE,
+	U3CgetGPSCoordinatesU3Ed__17_System_Collections_IEnumerator_get_Current_mB3A734F19EDB4ECA4D4943B08EA3A2A4E6820030,
+	Helper_degreesToRadians_m90682E512BBF866B7CF7AC81B5AD3BD6C28F3B6B,
+	Helper_radiansToDegrees_m0B1D570588853C6720035C4D75D90EC19CE1DB0C,
+	Helper_getNewGPSCoordinate_m77327B95A023F034A47BFBC654D96ABC6308F85C,
+	Helper_distanceBetweenTwoGPSCoordinates_m20E9BBD75BC72FEFFF344375A747F061516DD8B7,
+	Helper_getBearing_m6283597EDDF421069571C6DE0398A70FFB4C520D,
+	Helper_getElapsedSecondsFromUnixEpoch_m0EBAD1A27A9D87F6AE1C68D9397ABFE7FE834D8C,
+	UI_Start_m36F3E11677AB5677BD3A76B5865E90CD3609183A,
+	UI_Update_m7F9212980C571870E0700BAB400E09111370F6ED,
+	UI_displayTargetLocation_m3CD585034947786F9B68DE5B3232B9E3CB7EE297,
+	UI_displayDistanceFromTarget_m55E8E4562910E3C1FA5E6BF2E405AC008D92CA37,
+	UI_displayCurrentLocation_m4590C00079FBAABFC05667697A1912DA810C1B3F,
+	UI_rotateArrow_m2E42F991980B65EA55CDDFED91C6F12FA1B3DE94,
+	UI_displayCurrentDeviceOrientation_mEC974F1E8CCD61C31249E3BDE96BBACFDD82738D,
+	UI_toggleGreenSquare_mA2F16CF3402F9E407E95D0AE65214D7B748B66D6,
+	UI__ctor_m177FCA8E7C6A148BFF6FED9F758B3396F25FDFBB,
+};
+static const int32_t s_InvokerIndices[28] = 
+{
+	5797,
+	5706,
+	3914,
+	3914,
+	3914,
+	3817,
+	3914,
+	3115,
+	3914,
+	3749,
+	3817,
+	3914,
+	3817,
+	5458,
+	5458,
+	4457,
+	4351,
+	4351,
+	5789,
+	3914,
+	3914,
+	3914,
+	3914,
+	3914,
+	3914,
+	3914,
+	3085,
+	3914,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	28,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
---
+++ build_macos/Classes/Native/Unity.Profiling.Core_CodeGen.c
@@ -0,0 +1,72 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void Microsoft.CodeAnalysis.EmbeddedAttribute::.ctor()
+extern void EmbeddedAttribute__ctor_mC8F05DF619664423E825A1166C0F3A041D1241B1 (void);
+// 0x00000002 System.Void System.Runtime.CompilerServices.IsUnmanagedAttribute::.ctor()
+extern void IsUnmanagedAttribute__ctor_mC550690DDF4A9CAB011A3631E63E8C7E0A716753 (void);
+// 0x00000003 System.Void Unity.Profiling.ProfilerCounter`1::.ctor(Unity.Profiling.ProfilerCategory,System.String,Unity.Profiling.ProfilerMarkerDataUnit)
+// 0x00000004 System.Void Unity.Profiling.ProfilerCounter`1::Sample(T)
+// 0x00000005 System.Byte Unity.Profiling.ProfilerUtility::GetProfilerMarkerDataType()
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	EmbeddedAttribute__ctor_mC8F05DF619664423E825A1166C0F3A041D1241B1,
+	IsUnmanagedAttribute__ctor_mC550690DDF4A9CAB011A3631E63E8C7E0A716753,
+	NULL,
+	NULL,
+	NULL,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	3914,
+	3914,
+	0,
+	0,
+	0,
+};
+static const Il2CppTokenRangePair s_rgctxIndices[2] = 
+{
+	{ 0x02000004, { 0, 2 } },
+	{ 0x06000005, { 2, 1 } },
+};
+extern const uint32_t g_rgctx_ProfilerUtility_GetProfilerMarkerDataType_TisT_t8068902C2A117CE458BCABD925FAA62E1BABCF3D_m8627B0630DE28E77E8E67D2976AE2B50F23CBCAA;
+extern const uint32_t g_rgctx_UnsafeUtility_SizeOf_TisT_t8068902C2A117CE458BCABD925FAA62E1BABCF3D_m69E4AA485A50C96331283EEE2520E61F74AD0139;
+extern const uint32_t g_rgctx_T_t1CDECE2708A360FFD06302D16DA301344EAA4938;
+static const Il2CppRGCTXDefinition s_rgctxValues[3] = 
+{
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_ProfilerUtility_GetProfilerMarkerDataType_TisT_t8068902C2A117CE458BCABD925FAA62E1BABCF3D_m8627B0630DE28E77E8E67D2976AE2B50F23CBCAA },
+	{ (Il2CppRGCTXDataType)3, (const void *)&g_rgctx_UnsafeUtility_SizeOf_TisT_t8068902C2A117CE458BCABD925FAA62E1BABCF3D_m69E4AA485A50C96331283EEE2520E61F74AD0139 },
+	{ (Il2CppRGCTXDataType)1, (const void *)&g_rgctx_T_t1CDECE2708A360FFD06302D16DA301344EAA4938 },
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_Unity_Profiling_Core_CodeGenModule;
+const Il2CppCodeGenModule g_Unity_Profiling_Core_CodeGenModule = 
+{
+	"Unity.Profiling.Core.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	2,
+	s_rgctxIndices,
+	3,
+	s_rgctxValues,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
